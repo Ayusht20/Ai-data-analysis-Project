823b17d4f12e1b8b19b392be163afbc6dcfafe27
Ayusht20 <ayushtrilokchandani2005@gmail.com>
2026-04-18 14:39:59 +0530

    Updated API URL

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 # ---------------- CORS ----------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ai-data-analysis-project.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
